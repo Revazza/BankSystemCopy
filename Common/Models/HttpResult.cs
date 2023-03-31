eede0e109fd00b3b0476f4f946753c22a336bde9
Sandro Revazishvili <sandro.revazishviliii@gmail.com>
2023-03-31 11:19:34 +0400

    HttpResultAdded

---
+++ Common/Models/HttpResult.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+
+namespace BankSystem.Common.Models
+{
+    public enum HttpResultStatus
+    {
+        Ok = HttpStatusCode.OK,
+        InternalServerError = HttpStatusCode.InternalServerError,
+        Unauthorized = HttpStatusCode.Unauthorized,
+        BadRequest = HttpStatusCode.BadRequest,
+    }
+
+    public class HttpResult
+    {
+        public string Message { get; set; }
+        public HttpResultStatus Status { get; set; }
+
+        public Dictionary<string, string> Payload { get; set; }
+
+
+
+
+
+    }
+}
