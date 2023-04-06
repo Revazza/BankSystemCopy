e7364b81a12536fe5012f72ce42389744fd12407
Sandro Revazishvili <sandro.revazishviliii@gmail.com>
2023-04-06 21:43:39 +0400

    Currency converter fixed

---
+++ Common/Db/Dto/ExchangeRateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BankSystem.Common.Db.Dto
+{
+    public class ExchangeRateDto
+    {
+        public decimal ExchangeRate { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
